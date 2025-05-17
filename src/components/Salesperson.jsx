f4e4880ce9e1ba76fc3aae1495374905e8cb925f
Jayprox <jayadeguzman@gmail.com>
2025-05-17 13:03:14 -0700

    Adding Initial Salesperson Component

---
+++ src/components/Salesperson.jsx
@@ -0,0 +1,17 @@
+
+const Salesperson = () => {
+  
+    // Include functions here
+
+    return (
+        <div>
+            <h2>Salesperson Component</h2>
+            <text>Role: Person selling car at a car dealership</text>
+            <text>Primary Goal: Sell car at least 1% above list price, not including spiffs</text>
+            <text>Secondary Goal: Buyer does not "walk out" on the sale.</text>
+        </div>
+        
+    )
+}
+
+export default Salesperson
