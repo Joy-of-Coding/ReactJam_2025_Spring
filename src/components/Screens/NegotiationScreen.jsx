e91bd4ab992e86b514f890eabb0399c85032791e
andrecello <143760719+andrecello@users.noreply.github.com>
2025-05-20 10:44:48 -1000

    Created  NegotiationScreen.jsx
    Committing an early version of the negotiation screen. Working on the other concepts.

---
+++ src/components/Screens/NegotiationScreen.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CarList from "../CarList";
+import Car from "../Car";
+import Salesperson from "../Salesperson";
+import Buyer from "../Buyer";
+
+const NegotiationScreen = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NegotiationScreen
