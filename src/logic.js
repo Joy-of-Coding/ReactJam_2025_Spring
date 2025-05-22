5c9e4b519cbbd786eef4bf768197a05b7a0fcbe4
andrecello <143760719+andrecello@users.noreply.github.com>
2025-05-22 11:45:17 -1000

    modified  logic.js
    Added console logs.

--- src/logic.js
+++ src/logic.js
@@ -65,16 +65,27 @@ import cars from "./components/CarInfo";
 
 Rune.initLogic({
   minPlayers: 2,
-  maxPlayers: 3, //Buyer, Salesperson, Spectator
+  maxPlayers: 4, //Buyer, Salesperson, Spectator, Additional Player joining mid game
   setup: (allPlayerIds) => ({
     roles: Object.fromEntries(allPlayerIds.map(playerId => [playerId, null])),
     playerIds: allPlayerIds,
     cars: cars
   }),
+  // Assigns roles to players
   actions: {
     assignRole: (role, { game, playerId }) => {
       game.roles[playerId] = role;
     }
-  }
-})
+  },
+  
+  // Click the 'Add player' button on the desktop version succesfully adds a new player
+  events: {
+    playerJoined: (playerId, {game}) => {
+      console.log("Player joined:", playerId);
+      console.log("Current players:", game.playerIds);
+      console.log("Current roles:", game.roles); 
 
+    },
+    
+  
+}})
