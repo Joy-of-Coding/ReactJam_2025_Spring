160600e61f3b523db7f239badc0e25204dadc80a
Lisa Dean <lisardean82@gmail.com>
2025-05-29 17:59:03 -0400

    check images

---
+++ src/components/Screens/Carscreentest.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//import img1 from './assets/img/black_van.svg';
+//import img2 from './assets/img/green_bullet_mk2.svgge2.jpg';
+//import img3 from './assets/img/white_coupe.svg';
+import img4 from '../../assets/img/white_coupe.svg'
+const images = [img4];
+
+const Carscreentest = () => {
+  return (
+    
+    <div>
+      <h1>Gallery</h1>
+      {images.map((imgSrc, index) => (
+        <img key={index} src={imgSrc} alt={`Image ${index + 1}`} style={{ width: '200px', marginRight: '10px' }} />
+      ))}
+    </div>
+  );
+};
+
+export default Carscreentest;
