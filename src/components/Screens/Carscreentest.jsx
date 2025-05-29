7ba5cfc9c6b544901e2ba85075304bdf698689d1
Lisa Dean <lisardean82@gmail.com>
2025-05-29 18:24:00 -0400

    check images

--- src/components/Screens/Carscreentest.jsx
+++ src/components/Screens/Carscreentest.jsx
@@ -3,7 +3,15 @@ import img1 from '../../assets/img/black_van.svg';
 import img2 from '../../assets/img/green_van.svg';
 import img3 from '../../assets/img/white_coupe.svg';
 import img4 from '../../assets/img/sports_car.svg';
-const images = [img1,img2,img3,img4];
+import img5 from '../../assets/img/dreamstreamer.svg';
+import img6 from '../../assets/img/suv.svg';
+import img7 from '../../assets/img/van.svg';
+import img8 from '../../assets/img/spark.svg';
+import img9 from '../../assets/img/leafluxe_eco.svg';
+import img10 from '../../assets/img/green_bullet_mk2.svg';
+import img11 from '../../assets/img/red_comet_zr.svg';
+import img12 from '../../assets/img/gruntxl_v8.svg';
+const images = [img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,];
 
 const Carscreentest = () => {
   return (
