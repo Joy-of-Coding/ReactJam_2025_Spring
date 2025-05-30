83efcfcff78c18d26cf0d71e759c70ce35d3539d
KatrinaWright <katrina.elise.wright@gmail.com>
2025-05-29 22:04:47 -0500

    alphebetized developer credits

--- src/components/Screens/CreditsContent.jsx
+++ src/components/Screens/CreditsContent.jsx
@@ -26,26 +26,27 @@ const CreditsContent = () => {
 	return (
 		<div className="container">
 
- 				  <><b>TechJoy Software ReactJammers:</b>
+ 				  <>
+				    <b>TechJoy Software ReactJammers:</b>
 				    <br></br>
                     <br></br><b>John Walter Kowal</b>, Product Owner
                     <br></br><b>Linda Franklin (Linda Seriously)</b>, Project Manager
 					<br></br><b>Rebecca A. Stone</b>, Project Manager / Lead Designer
                     <br></br><b>Abu H Kamal</b>, Developer
-                    <br></br><b>Mindi Briese</b>, Developer
-                    <br></br><b>Lisa Dean</b>, Developer
-                    <br></br><b>Jay Deguzman</b>, Developer
-                    <br></br><b>Anna Rankin</b>, Developer
-                    <br></br><b>Tonia Ellers</b>, Developer / Tester
- 					<br></br><b>Rick Osteen</b>, JSON Developer
- 					<br></br><b>Andre "yamahacello"</b>, Developer
- 					<br></br><b>Kai Pannu</b>, Developer
  					<br></br><b>Alyssa Dannielle</b>, Developer
- 					<br></br><b>John Schlautman</b>, Developer/ AI Wrangler
+ 					<br></br><b>Andre "yamahacello"</b>, Developer
+                    <br></br><b>Anna Rankin</b>, Developer / Assistant Designer
+					<br></br><b>Jay Deguzman</b>, Developer
+					<br></br><b>John Schlautman</b>, Developer/ AI Wrangler
+					<br></br><b>Kai Pannu</b>, Developer
+                    <br></br><b>Lisa Dean</b>, Developer
+                    <br></br><b>Mindi Briese</b>, Developer 
+					<br></br><b>Rick Osteen</b>, Developer
+                    <br></br><b>Tonia Ellers</b>, Developer / Tester
 					<br></br><b>Hope Barnett</b>, Assistant Designer
  					<br></br><b>Michelle Evans (Shelly)</b>, Assistant Designer
 					<br></br><b>Dr. Susanne Atkinson</b>, Project Consultant
-				    <br></br><b>Katrina Wright</b>, Video Game Producer / Developer / AI wrangler
+				    <br></br><b>Katrina Wright</b>, Video Game Producer / Developer / AI Wrangler
                  </>
 
 		</div>
