712c17a1b3b48bf38d550dbc80f818c5643d9b68
KatrinaWright <katrina.elise.wright@gmail.com>
2025-05-29 19:58:21 -0500

    polished formatting

--- src/components/Screens/CreditsContent.jsx
+++ src/components/Screens/CreditsContent.jsx
@@ -25,34 +25,29 @@ const ReadMore = ({ children }) => {
 const CreditsContent = () => {
 	return (
 		<div className="container">
-			{/* <h2> */}
-								<ReadMore>
 
- 				  <>
-                     John Walter Kowal, Product Owner<br />
-                     Linda Franklin (Linda Seriously), Project Manager<br />
-                     Katrina Wright, Video Game Producer / Developer / AI wrangler<br />
-                     Abu H Kamal<br />
-                     Mindi Briese, Developer<br />
-                     Lisa Dean, officerjinxter<br />
-                     Hope Barnett, Assistant Designer<br />
-                     Rebecca A. Stone, Project Manager / Lead Designer<br />
-                     Jay Deguzman<br />
-                     Anna Rankin, Developer<br />
-                     Sam Crowe<br />
-                     Dr. Susanne Atkinson, Project Consultant<br />
-                     Tonia Ellers, Tester<br />
- 					Rick Osteen - JSON dev<br />
- 					Adre "yamahacello"
- 					Kai Pannu, Developer<br />
- 					Alyssa Dannielle<br />
- 					John Schlautman<br />
- 					Michelle Evans (Shelly), Assistant Designer
+ 				  <><b>TechJoy Software ReactJammers:</b>
+				    <br></br>
+                    <br></br><b>John Walter Kowal</b>, Product Owner
+                    <br></br><b>Linda Franklin (Linda Seriously)</b>, Project Manager
+					<br></br><b>Rebecca A. Stone</b>, Project Manager / Lead Designer
+                    <br></br><b>Abu H Kamal</b>, Developer
+                    <br></br><b>Mindi Briese</b>, Developer
+                    <br></br><b>Lisa Dean</b>, Developer
+                    <br></br><b>Jay Deguzman</b>, Developer
+                    <br></br><b>Anna Rankin</b>, Developer
+                    <br></br><b>Tonia Ellers</b>, Developer / Tester
+ 					<br></br><b>Rick Osteen</b>, JSON Developer
+ 					<br></br><b>Andre "yamahacello"</b>, Developer
+ 					<br></br><b>Kai Pannu</b>, Developer
+ 					<br></br><b>Alyssa Dannielle</b>, Developer
+ 					<br></br><b>John Schlautman</b>, Developer/ AI Wrangler
+					<br></br><b>Hope Barnett</b>, Assistant Designer
+ 					<br></br><b>Michelle Evans (Shelly)</b>, Assistant Designer
+					<br></br><b>Dr. Susanne Atkinson</b>, Project Consultant
+				    <br></br><b>Katrina Wright</b>, Video Game Producer / Developer / AI wrangler
                  </>
 
-
- 				</ReadMore>
-			{/* </h2> */}
 		</div>
 	);
 };
